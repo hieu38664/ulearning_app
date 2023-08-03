27ea67276680ad43f046983ed03d47a9df681cd6
Hiếu <haiphongtrhieu@gmail.com>
2023-08-03 18:47:43 +0700

    1

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        body: Container(
+          width: 375.w,
+        ),
+      ),
+    );
+  }
+}
